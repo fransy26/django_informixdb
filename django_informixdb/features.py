0d028a8415fe1c0f9cb810e52cd177290596ac5b
Fran Sy <fran.sy@reece.com.au>
2026-07-03 13:52:42 +0800

    cleanup

--- django_informixdb/features.py
+++ django_informixdb/features.py
@@ -22,7 +22,3 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     has_select_for_update = True
     supports_select_for_update_with_limit = True
     closed_cursor_error_class = InterfaceError
-
-    if django.VERSION >= (6, 0):
-        can_return_columns_from_insert = False
-        can_return_rows_from_update = False
--- django_informixdb/operations.py
+++ django_informixdb/operations.py
@@ -70,7 +70,7 @@ class DatabaseOperations(BaseDatabaseOperations):
         internal_type = expression.output_field.get_internal_type()
         if internal_type == 'BooleanField':
             converters.append(lambda value, *_: True if value == 1 else False)
-        elif internal_type == 'NullBooleanField' and django.VERSION < (6, 0):
+        elif internal_type == 'NullBooleanField':
             converters.append(lambda value, *_: True if value == 1 else False if value == 0 else None)
         elif internal_type == 'DateTimeField':
             converters.append(self.convert_datetimefield_value)
